15396658ddefd6a4cfade73153583f0aa99ecece
aquashdw <aquashdw@gmail.com>
2021-08-29 14:14:22 +0900

    add dtos

---
+++ apps/nest-axios-http/dto/post.body.dto.ts
@@ -0,0 +1,4 @@
+export class PostBodyDto {
+  // the response status for server to return
+  status: number;
+}
--- apps/nest-axios-http/src/app.controller.ts
+++ apps/nest-axios-http/src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PostBodyDto } from '../dto/post.body.dto';
+import { PostResDto } from '../dto/post.res.dto';
 
 @Controller()
 export class AppController {
@@ -11,7 +13,7 @@ export class AppController {
 
   // TODO relay get with path param
   @Get('get-path/:id')
-  getPath(@Param('id') id: number) {}
+  getPath(@Param('id') path: number) {}
 
   // TODO relay get with query param
   @Get('get-query')
@@ -19,17 +21,25 @@ export class AppController {
 
   // TODO response with server status response
   @Post('post-status')
-  postSimple() {}
+  postSimple(): PostResDto {
+    return null;
+  }
 
   // TODO request with request body
   @Post('post-body')
-  postBody() {}
+  postBody(@Body() dto: PostBodyDto) {
+    return null;
+  }
 
   // TODO response with predefined response body
   @Post('post-defined-res')
-  postDefinedResponse() {}
+  postDefinedResponse(@Body() dto: PostBodyDto): PostResDto {
+    return null;
+  }
 
   // TODO response with body directly from server
   @Post('post-dynamic-res')
-  postDynamicResponse() {}
+  postDynamicResponse(@Body() dto: PostBodyDto): any {
+    return null;
+  }
 }
